88063a6b7836aafa772de591480c62d1457c1016
Eshan Halekote <ehalekote@Eshans-MacBook-Pro-3.local>
2018-07-03 17:21:51 -0500

    new image test

--- js/canvas.js
+++ js/canvas.js
@@ -33,7 +33,7 @@ ctx = canvas.getContext('2d');
 ctx.font="20pt 'Roboto', sans-serif";
 ctx.textAlign = "center";
 ctx.fillStyle = "#302D2A";
-image.src = '/ArmadaProtocol/img/logo.png';
+image.src = 'https://upload.wikimedia.org/wikipedia/commons/c/c5/No_12_Squadron_Vengeance_dive_bomber_in_flight_during_December_1943.JPG';//'/ArmadaProtocol/img/logo.png';
 ctx.fillText("Bundling the world's digital assets",window.innerWidth/2,415);
 
 
